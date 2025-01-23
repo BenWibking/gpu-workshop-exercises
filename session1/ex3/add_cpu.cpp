c0ef91fb710527b9dcaed2467176b5e5f635bc48
Ben Wibking <ben@wibking.com>
2025-01-23 15:35:50 -0500

    add gpu version

---
+++ session1/ex3/add_cpu.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+
+// function to add the elements of two arrays
+void add(int n, float *x, float *y)
+{
+  for (int i = 0; i < n; i++)
+      y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+  int N = 1e6; // 1 million elements
+
+  std::vector<float> x(N);
+  std::vector<float> y(N);
+
+  // initialize x and y arrays
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+  // Run kernel on 1M elements on the CPU
+  add(N, x.data(), y.data());
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++) {
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  }
+  std::cout << "Max error: " << maxError << std::endl;
+
+  return 0;
+}
---
+++ session1/ex3/add_gpu.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <math.h>
+
+__global__
+void add(int n, float *x, float *y)
+{
+  for (int i = 0; i < n; i++)
+      y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+  int N = 1e6;
+
+  std::vector<float> x(N);
+  std::vector<float> y(N);
+
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+  float *x_d;
+  float *y_d;
+  cudaMalloc(&x_d, N*sizeof(float));
+  cudaMalloc(&y_d, N*sizeof(float));
+
+  cudaMemcpy(x_d, x, N*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(y_d, y, N*sizeof(float), cudaMemcpyHostToDevice);
+
+  add<<<1, 1>>>(N, x, y);
+
+  cudaDeviceSynchronize();
+
+  cudaMemcpy(y, y_d, N*sizeof(float), cudaMemcpyDeviceToHost);
+
+  cudaFree(x_d);
+  cudaFree(y_d);
+
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
+
+  return 0;
+}
